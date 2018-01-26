c54e74a9e5b0c7f6a693a79a6336dac4d07885e3
Pedro Baumann <ondoheer@gmail.com>
2018-01-26 10:20:55 -0500

    now with a templating inheritance mechanism

--- gowiki.go
+++ gowiki.go
@@ -4,30 +4,105 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"html/template"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"path/filepath"
 	"regexp"
+
+	"github.com/oxtoacart/bpool" // A common use case for this package is to use buffers to execute HTML templates against (via ExecuteTemplate)
+	//or encode JSON into (via json.NewEncoder).
+	//This allows you to catch any rendering or marshalling errors prior to writing to a http.ResponseWriter,
+	//which helps to avoid writing incomplete or malformed data to the response.
 )
+
+// var templateBaseDir = "templates"
+var dataBaseDir = "data"
+
+var templates map[string]*template.Template
+var bufpool *bpool.BufferPool
 
 type Page struct {
 	Title string
 	Body  []byte
 }
 
-var templateBaseDir = "templates"
-var dataBaseDir = "data"
+type TemplateConfig struct {
+	TemplateLayoutPath  string
+	TemplateIncludePath string
+}
 
-func templateRoutes() []string {
+var mainTempl = `{{define "main" }} {{ template "base" . }} {{ end }}`
+var templateConfig TemplateConfig
 
-	paths := []string{
+func loadConfiguration() {
+	templateConfig.TemplateLayoutPath = "templates/layouts/"
+	templateConfig.TemplateIncludePath = "templates/"
+}
 
-		filepath.Join(templateBaseDir, "edit.html"),
-		filepath.Join(templateBaseDir, "view.html"),
+func loadTemplates() {
+	if templates == nil {
+		templates = make(map[string]*template.Template)
 	}
 
-	return paths
+	layoutFiles, err := filepath.Glob(templateConfig.TemplateLayoutPath + "*.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	includeFiles, err := filepath.Glob(templateConfig.TemplateIncludePath + "*.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mainTemplate := template.New("main")
+
+	mainTemplate, err = mainTemplate.Parse(mainTempl)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range includeFiles {
+		fileName := filepath.Base(file)
+		files := append(layoutFiles, file)
+
+		templates[fileName], err = mainTemplate.Clone()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		templates[fileName] = template.Must(templates[fileName].ParseFiles(files...))
+	}
+	log.Println("Templates loades successfully")
+
+	bufpool = bpool.NewBufferPool(64)
+	log.Println("buffer allocation succesful")
+
+}
+
+func renderTemplate(w http.ResponseWriter, name string, data interface{}) {
+	tmpl, ok := templates[name]
+
+	if !ok {
+		http.Error(w, fmt.Sprintf("the template %s does not exist", name),
+			http.StatusInternalServerError)
+	}
+
+	buf := bufpool.Get()
+	defer bufpool.Put(buf)
+
+	err := tmpl.Execute(buf, data)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	buf.WriteTo(w)
 }
 
 func generateArticlePath(title string) string {
@@ -35,7 +110,6 @@ func generateArticlePath(title string) string {
 }
 
 // Globals
-var templates = template.Must(template.ParseFiles(templateRoutes()...))
 
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
@@ -74,14 +148,8 @@ func loadPage(title string) (*Page, error) {
 
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index.html", nil)
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
@@ -94,7 +162,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 		return
 	}
 
-	renderTemplate(w, "view", p)
+	renderTemplate(w, "view.html", p)
 
 }
 
@@ -104,7 +172,7 @@ func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	if err != nil {
 		p = &Page{Title: title}
 	}
-	renderTemplate(w, "edit", p)
+	renderTemplate(w, "edit.html", p)
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
@@ -134,6 +202,10 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 
 func main() {
 
+	loadConfiguration()
+	loadTemplates()
+
+	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
